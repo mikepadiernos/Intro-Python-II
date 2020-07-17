631360b71847c1a458f8f1f57f87531c79881db9
mikepadiernos <mike.padiernos@gmail.com>
2020-07-16 23:13:10 -0400

    [FEATURE] class / items
    - started code for item & inventory

--- src/item.py
+++ src/item.py
@@ -1,8 +1,8 @@
 class Item:
-    def __init__(self, item_type, item_attributes, item_name):
+    def __init__(self, name, item_type, item_attributes):
+        self.name = name
         self.type = item_type
         self.attributes = item_attributes
-        self.name = item_name
 
     def __str__(self):
         if len(self.name) > 0:
--- src/player.py
+++ src/player.py
@@ -1,16 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-from room import Room
 
 
 def blocked():
     print(f'Thou art blocked!')
 
 
-class Player(Room):
-    def __init__(self, name, room):
+class Player:
+    def __init__(self, name, room, inventory):
         self.name = name
         self.current_room = room
+        self.inventory = inventory
 
     def movement(self, direction):
         here = self.current_room.movements(direction)
